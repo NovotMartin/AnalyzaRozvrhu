e9f6ff483fc13b022a91472019838322858b684b
JaTiNevim <jan.chvojan92@gmail.com>
2017-07-04 14:19:21 +0200

    Úprava třídy ZatezNaStudenta, vytvoření generátoru výstupu pro SRA.

--- AnalyzaRozvrhu/STAG_ReportGenerator.cs
+++ AnalyzaRozvrhu/STAG_ReportGenerator.cs
@@ -86,5 +86,58 @@ namespace AnalyzaRozvrhu
                 excel.Save();
             }
         }
+
+        public static void GenerovatSRAPrehledXLS(this STAG_Classes.STAG_Database data, string name)
+        {
+            FileInfo file;
+            int number = 2;
+            if ((file = new FileInfo(name + ".xlsx")).Exists)
+                while ((file = new FileInfo(name + number + ".xlsx")).Exists)
+                    number++;
+
+            using (var excel = new ExcelPackage(file))
+            {
+                ExcelWorksheet sheet;
+
+                var deprts = data.HiearchiePracovist[data.Fakulta];
+                double per;
+                int row = 2;
+                int col = 5;
+
+                sheet = excel.Workbook.Worksheets.Add("sheet");
+                sheet.Cells[1, 1].Value = "Osobni cislo";
+                sheet.Cells[1, 2].Value = "Rocnik";
+                sheet.Cells[1, 3].Value = "St. program";
+                sheet.Cells[1, 4].Value = "Forma";
+                foreach (var dep in deprts)
+                    sheet.Cells[1, col++].Value = dep.Key;
+                sheet.Cells[1, col].Value = "jiná";
+
+                foreach (var student in data.zatezNaStudenta.GetAll())
+                {
+                    sheet.Cells[row, 1].Value = student.Item1.OsCislo;
+                    sheet.Cells[row, 2].Value = student.Item1.Rocnik;
+                    sheet.Cells[row, 3].Value = student.Item1.KodSp;
+                    sheet.Cells[row, 4].Value = student.Item1.FormaSp;
+                    col = 5;
+                    foreach (var dep in deprts)
+                    {
+                        if (student.Item2.TryGetValue(dep.Key, out per))
+                            sheet.Cells[row, col++].Value = per;
+                        else
+                            sheet.Cells[row, col++].Value = 0;
+                    }
+                    sheet.Cells[row, col].Value = (from cizi in student.Item2 where !deprts.Keys.Contains(cizi.Key) select cizi.Value).Sum();
+                    row++;
+                }
+                for (int i = 1; i <= 4; i++)
+                {
+                    ExcelColumn coll = sheet.Column(i);
+                    coll.AutoFit();
+                    coll.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                }
+                excel.Save();
+            }
+        }
     }
 }
--- AnalyzaRozvrhu/ZatezNaStudenta.cs
+++ AnalyzaRozvrhu/ZatezNaStudenta.cs
@@ -8,18 +8,17 @@ namespace AnalyzaRozvrhu
 {
     /// <summary>
     /// Zatez kateder na vyuku studentu.
-    /// Pozn.: Zatez neni normalizovana.
     /// </summary>
     public class ZatezNaStudenta
     {
-        private Dictionary<string, Dictionary<string, double>> students;
+        private Dictionary<STAG_Classes.Student, Dictionary<string, double>> students;
 
         /// <summary>
         /// Inicializace.
         /// </summary>
         public ZatezNaStudenta()
         {
-            students = new Dictionary<string, Dictionary<string, double>>();
+            students = new Dictionary<STAG_Classes.Student, Dictionary<string, double>>();
         }
 
         /// <summary>
@@ -28,7 +27,7 @@ namespace AnalyzaRozvrhu
         /// <param name="student">Osobní číslo studenta FXXYYY</param>
         /// <param name="katedra">Zkratka katedry</param>
         /// <param name="zatez">Zatížení studenta</param>
-        public void Pridat(string student, string katedra, double zatez)
+        public void Pridat(STAG_Classes.Student student, string katedra, double zatez)
         {
             if (students.ContainsKey(student))
                 if (students[student].ContainsKey(katedra))
@@ -40,12 +39,80 @@ namespace AnalyzaRozvrhu
         }
 
         /// <summary>
-        /// Vyčiští všechny uložené zátěže
+        /// Zmeni zates studenta na konkretni katedru
         /// </summary>
-        public void clean()
+        /// <param name="student"></param>
+        /// <param name="katedra"></param>
+        /// <param name="zatez"></param>
+        /// <returns></returns>
+        public bool Zmenit(STAG_Classes.Student student, string katedra, double zatez)
         {
-            students = new Dictionary<string, Dictionary<string, double>>();
+            Smazat(student, katedra);
+            Pridat(student, katedra, zatez);
+            return true;
         }
 
+        /// <summary>
+        /// Vyčiští všechny uložené zátěže
+        /// </summary>
+        public void Smazat()
+        {
+            students = new Dictionary<STAG_Classes.Student, Dictionary<string, double>>();
+        }
+
+        /// <summary>
+        /// Smazat zatizeni studenta
+        /// </summary>
+        /// <param name="student"></param>
+        public bool Smazat(STAG_Classes.Student student)
+        {
+            if (students.ContainsKey(student))
+                return students.Remove(student);
+            return false;
+        }
+
+        /// <summary>
+        /// Zmeni zatizeni studenta kontretni katedry
+        /// </summary>
+        /// <param name="student"></param>
+        /// /// <param name="katedra"></param>
+        public bool Smazat(STAG_Classes.Student student, string katedra)
+        {
+            if (students.ContainsKey(student))
+                if (students[student].ContainsKey(katedra))
+                    return students[student].Remove(katedra);
+            return false;
+        }
+
+        /// <summary>
+        /// Ziskani zateze konkretniho studenta
+        /// </summary>
+        /// <returns>Slovnik Katedra -> zatez</returns>
+        public Dictionary<string, double> ZiskatZatezStudenta(STAG_Classes.Student student)
+        {
+            Dictionary<string, double> ret;
+            if (students.TryGetValue(student, out ret))
+                return Normalize(ret);
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Postupne ziskani vsech studentu, kterym byla pridana zatez
+        /// </summary>
+        /// <returns>Vrací pár: objekt studenta a slovnik katedra -> zatez</returns>
+        public IEnumerable<Tuple<STAG_Classes.Student, Dictionary<string, double>>> GetAll()
+        {
+            foreach (var student in students)
+                yield return new Tuple<STAG_Classes.Student, Dictionary<string, double>>(student.Key, ZiskatZatezStudenta(student.Key));
+        }
+
+        private Dictionary<string, double> Normalize(Dictionary<string, double> percents)
+        {
+            double sum = (from dep in percents select dep.Value).Sum();
+            for (int i = 0; i < percents.Count; i++)
+                percents[percents.ElementAt(i).Key] /= sum;
+            return percents;
+        }
     }
 }
